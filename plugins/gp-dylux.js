895632801b0c13c67ad7bb4035729119e9de9a1a
botextreme <104403771+botextreme@users.noreply.github.com>
2022-05-29 15:52:42 -0500

    Update gp-dylux.js

--- plugins/gp-dylux.js
+++ plugins/gp-dylux.js
@@ -3,19 +3,22 @@ let handler = async m => m.reply(`
 ≡  *ESCORPION  ┃ ᴮᴼᵀ*  
 
 ▢ GRUPOS
+*ESCORPION-BOT*
  1.- https://chat.whatsapp.com/LN1dF710mLiJjEpzF1vq24
  2.- https://chat.whatsapp.com/JWHICxal9Ra25vGxuJqQ0s
-
+*NECOSMIC-BOT*
+ 1.- https://chat.whatsapp.com/JfjdCJLu7bvJnm37uy07ns
+ 
 ▢ *Grupo Telegram*
  https://t.me/
 
 ▢ *YouTube*
-• https://www.youtube.com/dinocomedia
+• Dinocomedia
 
 
 `.trim())
-handler.help = ['gpdylux']
+handler.help = ['gpescorpion']
 handler.tags = ['main']
-handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux'] 
+handler.command = ['grupos', 'groupescorpion', 'escorpiongp', 'escorgp', 'gpescorpion'] 
 
 export default handler
