0715642326941cc9767e2123d119c73a7057c823
botextreme <104403771+botextreme@users.noreply.github.com>
2022-05-29 20:43:33 -0500

    Update gp-dylux.js

--- plugins/gp-dylux.js
+++ plugins/gp-dylux.js
@@ -7,7 +7,7 @@ let handler = async m => m.reply(`
  1.- https://chat.whatsapp.com/LN1dF710mLiJjEpzF1vq24
  2.- https://chat.whatsapp.com/JWHICxal9Ra25vGxuJqQ0s
 *NECOSMIC-BOT*
- 1.- https://chat.whatsapp.com/JfjdCJLu7bvJnm37uy07ns
+ 1.- https://chat.whatsapp.com/FR1N45RJOLh4BcAvUyssLk
  
 ▢ *Grupo Telegram*
  https://t.me/
